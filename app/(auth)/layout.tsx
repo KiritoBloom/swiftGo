670b7f914fb5f203a199c133f863fb305a05e250
Mohamed Ahmed <knightwing862@gmail.com>
2024-02-27 00:33:27 +0200

    Auth page UI Changes

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -1,6 +1,37 @@
+import Image from "next/image";
+
 const AuthLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <div className="flex justify-center items-center h-full">{children}</div>
+    <div className="justify-center items-center h-full mr-20 relative w-full">
+      <div className="bg-gray-200 w-full h-full relative text-black">
+        <Image
+          src="/Logo.png"
+          alt="SwiftGo-Logo"
+          width={150}
+          height={150}
+          className="ml-5 pt-2"
+        />
+        <div className="flex flex-col justify-center items-center mt-[40%] md:mt-[40%] lg:mt-[15%] text-center">
+          <h1 className="text-4xl font-semibold">
+            🛒 Ready to elevate your shopping experience? Sign up or log in now.
+            Your personalized retail journey starts here.
+          </h1>
+          <h2 className="text-lg mt-5">
+            🚀 Swift Go: Elevate Your Everyday - Your Ultimate Destination for
+            Daily Essentials! 🚀
+          </h2>
+          <p className="font-bold mt-5 underline">
+            <a href="#formContainer">Scroll Down To get started</a>
+          </p>
+        </div>
+      </div>
+      <div
+        className="flex justify-center items-center pb-10 bg-gray-200"
+        id="formContainer"
+      >
+        {children}
+      </div>
+    </div>
   );
 };
 
